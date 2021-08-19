cf844038b0093e2062f5fba6bfd58a1bb4414aba
Nguyen Lam Truong <nguyenlamtruong20021@gmail.com>
2021-08-19 10:51:48 +0700

    alo

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,11 @@
-Clickyes = function() {
-    alert("Biết ngay mà :v");
-}
+clickYes = function() {
+    alert("Biết thế nào cũng chọn có mà =)))");
+};
+
+clickNo = function() {
+   var btn = document.getElementById('btn-no');
+   var top = Math.floor(Math.random()*800);
+   var left = Math.floor(Math.random()*400+600);
+   btn.style.top = top + 'px';
+   btn.style.left = left + 'px';
+};
