91a388b7880befd91afe4e203705638cd95bd643
Nguyen Lam Truong <nguyenlamtruong20021@gmail.com>
2021-08-31 13:32:14 +0700

    sua~ file

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,18 +1,1 @@
-clickYes = function() {
-    alert("Biết thế nào cũng chọn có mà =)))");
-};
-
-clickNo = function() {
-   var btn = document.getElementById('btn-no');
-   var top = Math.floor(Math.random()*800);
-   var left = Math.floor(Math.random()*400+600);
-   btn.style.top = top + 'px';
-   btn.style.left = left + 'px';
-<<<<<<< HEAD
-};
-=======
-=======
-clickYes = function() {
-    alert("Biết thế nào cũng chọn có mà =)))");
-};
->>>>>>> bbdcee058df924b167126fb3d50da4dacd0e351e
+//
